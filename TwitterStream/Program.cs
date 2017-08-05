7ab5d25a35e78d6540f6813cac0a643e9bd29776
Ailuridaes <ailuridaes@gmail.com>
2017-08-04 19:31:08 -0700

    Stream tweets using tweetinvi library

---
+++ TwitterStream/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Tweetinvi;
+
+namespace TwitterStream {
+    class Program {
+        static void Main(string[] args) {
+
+            // Set up twitter credentials (https://apps.twitter.com)
+            Auth.SetUserCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+
+            var stream = Stream.CreateFilteredStream();
+            stream.AddTrack("cats");
+            stream.MatchingTweetReceived += (sender, eventArgs) => {
+                Console.WriteLine("A tweet about cats has been found; the tweet is '" + eventArgs.Tweet.ToJson() + "'");
+            };
+            stream.StartStreamMatchingAllConditions();
+        }
+    }
+}
